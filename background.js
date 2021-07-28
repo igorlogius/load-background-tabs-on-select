2715995c4da9b39ef59a0854523e48292f8984d7
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-07-28 21:30:48 +0200

    fix(#1): ignore hidden tabs, which might interfere with 'Simple Tab Groups'

--- background.js
+++ background.js
@@ -1,25 +1,25 @@
 
-let was_activ_once = new Set();
+let wasActive = new Set();
 
 browser.tabs.onUpdated.addListener( (tabId,changeInfo,tab) => {
 
-	// 
-	if(was_activ_once.has(tabId)){ return; }
-
-	if(tab.active) { return; }
-
-	if(tab.discarded){ return; }
+	// ignore
+	if(tab.active) { return; }   
+	if(tab.hidden) { return; }    
+	if(tab.discarded) { return; } 
+	if(wasActive.has(tabId)){ return; } 
 
 	browser.tabs.discard(tabId);
+
 }, {properties: ["status"]});
 
 
 browser.tabs.onActivated.addListener( (activeInfo) => {
-	was_activ_once.add(activeInfo.tabId);
+	wasActive.add(activeInfo.tabId);
 });
 
 browser.tabs.onRemoved.addListener( (tabId, removeInfo) => {
-	was_activ_once.delete(tabId);
+	wasActive.delete(tabId);
 });
 
 
