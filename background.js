759f83bcab5aa1e44eb2051d3c4bdc3cabb24ddd
igorlogius <67047467+igorlogius@users.noreply.github.com>
2022-10-22 23:49:40 +0200

    chore: a little code cleanup

--- background.js
+++ background.js
@@ -4,23 +4,26 @@ let wasActive = new Set();
 
 browser.tabs.onUpdated.addListener( (tabId,changeInfo,tab) => {
 
-	// ignore
-	if(tab.active) { return; }
-	if(tab.hidden) { return; }
-	if(tab.discarded) { return; }
-	if(wasActive.has(tabId)){ return; }
-
-	browser.tabs.discard(tabId);
+    // ignore
+    if( !(tab.active ||
+          tab.hidden ||
+          tab.discarded ||
+          wasActive.has(tabId))
+    ){
+        browser.tabs.discard(tabId);
+    }
 
 }, {properties: ["status"]});
 
-
 browser.tabs.onActivated.addListener( (activeInfo) => {
-	wasActive.add(activeInfo.tabId);
+    if(!wasActive.has(activeInfo.tabId)){
+        wasActive.add(activeInfo.tabId);
+    }
 });
 
-browser.tabs.onRemoved.addListener( (tabId/*, removeInfo*/) => {
-	wasActive.delete(tabId);
+browser.tabs.onRemoved.addListener( tabId => {
+    if(wasActive.has(tabId)){
+        wasActive.delete(tabId);
+    }
 });
-
 
